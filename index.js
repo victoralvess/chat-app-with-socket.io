44bb17860242118342bc83afadf675c97196458a
Victor Alves <vsilva.programador@gmail.com>
2018-03-30 09:43:15 -0300

    Simple Chat Done

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello World</h1>');
+	// res.send('<h1>Hello World</h1>');
+	res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+	console.log('A user connected.');
+	
+	socket.on('disconnect', () => {
+		console.log('A user disconnected.');
+	});
+
+	socket.on('message', (message) => {
+		console.log('Message: ', message);
+		io.emit('message', message);
+	});
+
+});
+
+server.listen(3000, () => {
 	console.log('Running at http://localhost:3000');
 });
