884be0c1c5d2db4a240af1d01175fdd102bd7cd9
Victor Alves <vsilva.programador@gmail.com>
2018-03-31 11:23:32 -0300

    update socket logic on back-end

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
-	// res.send('<h1>Hello World</h1>');
 	res.sendFile(`${__dirname}/index.html`);
 });
 
 io.on('connection', (socket) => {
-	console.log('A user connected.');
-	// io.emit('connected', 'A new user connected.');
-	
+	console.log('A user connected.', socket.handshake.query.user);
+	const user = JSON.parse(socket.handshake.query.user);
+
+	io.emit('user', {
+		...user,
+		id: socket.id
+	});
+
 	socket.on('disconnect', () => {
 		console.log('A user disconnected.');
+		io.emit('user_disconnected', socket.id);
 	});
 
 	socket.on('message', (message) => {
 		console.log('Message: ', message);
 		io.emit('message', message);
+	});
+
+	socket.on('me', (user) => {
+		io.emit('user', user);
 	});
 
 });
