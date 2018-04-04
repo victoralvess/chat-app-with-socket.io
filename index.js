a482bcf5efcce5eb9bd24241091e1f8951350e13
Victor Alves <vsilva.programador@gmail.com>
2018-04-04 10:01:22 -0300

    Removing routing because of the public folder

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/index.html`);
-});
+app.use(express.static('public'));
 
 io.on('connection', socket => {
 	console.log('A user connected.', socket.handshake.query.user);
